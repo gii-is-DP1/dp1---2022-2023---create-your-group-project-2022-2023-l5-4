572f3c7d3d860f855bbe7c3ccae17df7a6bb90d6
marzoiras <marzoiras@alum.us.es>
2022-11-16 18:10:01 +0100

    actualización

--- src/main/java/org/springframework/samples/dobble/card/Card.java
+++ src/main/java/org/springframework/samples/dobble/card/Card.java
@@ -1,6 +1,8 @@
 package org.springframework.samples.dobble.card;
 
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,9 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.validation.constraints.Size;
+
 
 import org.springframework.samples.dobble.symbol.Symbol;
+
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/org/springframework/samples/dobble/card/CardRepository.java
+++ src/main/java/org/springframework/samples/dobble/card/CardRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CardRepository extends CrudRepository<Card, Integer> { 
+public interface CardRepository extends CrudRepository<Card, Long> { 
     
 }
