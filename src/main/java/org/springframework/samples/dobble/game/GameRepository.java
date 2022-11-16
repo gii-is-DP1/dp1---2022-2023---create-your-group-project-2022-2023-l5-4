ab69df0bd5ecd1879d4e0e435ea5a744ecd764f0
antlopcub <antlopcub@alum.us.es>
2022-11-17 00:43:13 +0100

    H3 casi completa

--- src/main/java/org/springframework/samples/dobble/game/GameRepository.java
+++ src/main/java/org/springframework/samples/dobble/game/GameRepository.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.dobble.game;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface GameRepository extends CrudRepository<Game,Long> {
-    
+    List<Game> findAll();
 }
