e205c614d0e50620d46d9e6e176dcf35c9e3a409
bloxio3001 <adrianporrasazahares@gmail.com>
2022-12-08 17:34:27 +0100

    Added compatibility with different Symbol Sets

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -11,130 +11,83 @@ INSERT INTO authorities(id,username,authority) VALUES (5,'adrgarpor','admin');
 INSERT INTO users(username,password,enabled) VALUES ('edurobrus','0wn3r',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (6,'edurobrus','owner');
 
--- One vet user, named vet1 with passwor v3t
-INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
-INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
+-- All 55 cards --
+INSERT INTO cards(id) VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28),(29),(30),(31),(32),
+(33),(34),(35),(36),(37),(38),(39),(40),(41),(42),(43),(44),(45),(46),(47),(48),(49),(50),(51),(52),(53),(54),(55);
 
-INSERT INTO vets(id, first_name,last_name) VALUES (1, 'James', 'Carter');
-INSERT INTO vets(id, first_name,last_name) VALUES (2, 'Helen', 'Leary');
-INSERT INTO vets(id, first_name,last_name) VALUES (3, 'Linda', 'Douglas');
-INSERT INTO vets(id, first_name,last_name) VALUES (4, 'Rafael', 'Ortega');
-INSERT INTO vets(id, first_name,last_name) VALUES (5, 'Henry', 'Stevens');
-INSERT INTO vets(id, first_name,last_name) VALUES (6, 'Sharon', 'Jenkins');
+-- All 57 symbols --
+INSERT INTO symbols(id) VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20),(21),(22),(23),(24),(25),(26),(27),(28),(29),(30),(31),(32),
+(33),(34),(35),(36),(37),(38),(39),(40),(41),(42),(43),(44),(45),(46),(47),(48),(49),(50),(51),(52),(53),(54),(55),(56),(57);
 
-INSERT INTO specialties VALUES (1, 'radiology');
-INSERT INTO specialties VALUES (2, 'surgery');
-INSERT INTO specialties VALUES (3, 'dentistry');
+-- symbolsets, at least 'original' must be set --
+INSERT INTO symbolsets(id,name,path) VALUES (1,'original', 'resources/images/symbols/original');
 
-INSERT INTO vet_specialties VALUES (2, 1);
-INSERT INTO vet_specialties VALUES (3, 2);
-INSERT INTO vet_specialties VALUES (3, 3);
-INSERT INTO vet_specialties VALUES (4, 2);
-INSERT INTO vet_specialties VALUES (5, 1);
+-- symbolvariants for 'original' set --
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (1, 'Anchor','anchor.png',1,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (2, 'Apple','apple.png',2,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (3, 'Baby Bottle','baby-bottle.png',3,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (4, 'Bomb','bomb.png',4,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (5, 'cactus','cactus.png',5,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (6, 'Candle','candle.png',6,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (7, 'Taxi','taxi.png',7,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (8, 'Carrot','carrot.png',8,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (9, 'Chess Knight','chess-knight',9,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (10, 'Clock','clock.png',10,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (11, 'Clown','clown.png',11,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (12, 'Daisy flower','daisy-flower.png',12,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (13, 'Dinosaur','dinosaur.png',13,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (14, 'Dolphin','dolphin.png',14,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (15, 'Dragon','dragon.png',15,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (16, 'Exclamation Point','exclamation-point.png',16,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (17,'Eye','eye.png',17,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (18, 'Fire','fire.png',18,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (19, 'Four Leaf Clover','four-leaf-clover.png',19,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (20, 'Fantasy','fantasy.png',20,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (21, 'Green Splats','green-splats.png',21,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (22, 'Hammer','hammer.png',22,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (23, 'Heart','heart.png',23,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (24, 'Ice Cube','ice-cube.png',24,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (25, 'Igloo','igloo.png',25,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (26, 'Key','key.png',26,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (27, 'Ladybug','ladybug.png',27,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (28, 'Light Bulb','light-bulb.png',28,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (29, 'Lightning Bolt','lightning-bolt.png',29,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (30, 'Lock','lock.png',30,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (31, 'Mapple Leaf','mapple-leaf.png',31,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (32, 'Moon','moon.png',32,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (33, 'No Entry Sign','no-entry-sign.png',33,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (34, 'Orange Scarecrow Man','orange-scarecrow-man.png',34,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (35, 'Pencil','pencil.png',35,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (36, 'Purple Bird','bird.png',36,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (37, 'Purple Cat','cat.png',37,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (38, 'Purple Dobble Hand','dobble-hand.png',38,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (39, 'Red Lips','red-rips.png',39,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (40, 'Scissors','scissors.png',40,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (41, 'Skull and Crossbones','skull-and-crossbones.png',41,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (42, 'Snowflake','snowflake.png',42,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (43, 'Snowman','snowman.png',43,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (44, 'Spider','spider.png',44,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (45, 'Spider Web','spider-web.png',45,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (46, '​​Sun','sun.png',46,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (47, 'Sunglasses','sunglasses.png',47,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (48, 'Target','target.png',48,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (49, 'Tortoise','tortoise.png',49,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (50, 'Treble Clef','treble-clef.png',50,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (51, 'Tree','tree.png',51,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (52, 'Water Drip','water-drip.png',52,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (53, 'Dog','dog.png',53,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (54, 'Yin and Yang','yin-and-yang.png',54,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (55, 'Zebra','zebra.png',55,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (56, 'Question Mark','question-mark.png',56,1);
+INSERT INTO symbolvariants(id, name, file_name, symbol_id,symbol_set_id) VALUES (57, 'cheese','cheese.png',57,1);
 
-INSERT INTO types VALUES (1, 'cat');
-INSERT INTO types VALUES (2, 'dog');
-INSERT INTO types VALUES (3, 'lizard');
-INSERT INTO types VALUES (4, 'snake');
-INSERT INTO types VALUES (5, 'bird');
-INSERT INTO types VALUES (6, 'hamster');
-INSERT INTO types VALUES (7, 'turtle');
-
-INSERT INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023', 'owner1');
-INSERT INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749', 'owner1');
-INSERT INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763', 'owner1');
-INSERT INTO owners VALUES (4, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198', 'owner1');
-INSERT INTO owners VALUES (5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765', 'owner1');
-INSERT INTO owners VALUES (6, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654', 'owner1');
-INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387', 'owner1');
-INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683', 'owner1');
-INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435', 'owner1');
-INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487', 'owner1');
-INSERT INTO owners VALUES (11, 'Antonio', 'Barea', 'Tarfia St.', 'Sevilla', '662934416', 'antbarjim1');
-
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (1, 'Leo', '2010-09-07', 1, 1);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (2, 'Basil', '2012-08-06', 6, 2);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (3, 'Rosy', '2011-04-17', 2, 3);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (4, 'Jewel', '2010-03-07', 2, 3);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (5, 'Iggy', '2010-11-30', 3, 4);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (6, 'George', '2010-01-20', 4, 5);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (7, 'Samantha', '2012-09-04', 1, 6);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (8, 'Max', '2012-09-04', 1, 6);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (9, 'Lucky', '2011-08-06', 5, 7);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (10, 'Mulligan', '2007-02-24', 2, 8);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '2010-03-09', 5, 9);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
-INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (14, 'Firulais', '2011-07-10', 2, 11);
-
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
-
-INSERT INTO symbols(id, name) VALUES (1, 'Anchor');
-INSERT INTO symbols(id, name) VALUES (2, 'Apple');
-INSERT INTO symbols(id, name) VALUES (3, 'Bottle');
-INSERT INTO symbols(id, name) VALUES (4, 'pump');
-INSERT INTO symbols(id, name) VALUES (5, 'cacti');
-INSERT INTO symbols(id, name) VALUES (6, 'Candle');
-INSERT INTO symbols(id, name) VALUES (7, 'Taxis');
-INSERT INTO symbols(id, name) VALUES (8, 'Carrot');
-INSERT INTO symbols(id, name) VALUES (9, 'Chess Knight');
-INSERT INTO symbols(id, name) VALUES (10, 'Clock');
-INSERT INTO symbols(id, name) VALUES (11, 'Clown');
-INSERT INTO symbols(id, name) VALUES (12, 'daisy flower');
-INSERT INTO symbols(id, name) VALUES (13, 'Dinosaurs');
-INSERT INTO symbols(id, name) VALUES (14, 'Dolphin');
-INSERT INTO symbols(id, name) VALUES (15, 'Dragon');
-INSERT INTO symbols(id, name) VALUES (16, 'exclamation point');
-INSERT INTO symbols(id, name) VALUES (17,'Eye');
-INSERT INTO symbols(id, name) VALUES (18, 'Fire');
-INSERT INTO symbols(id, name) VALUES (19, 'Four Leaf Clover');
-INSERT INTO symbols(id, name) VALUES (20, 'Fantasy');
-INSERT INTO symbols(id, name) VALUES (21, 'Green symbolss');
-INSERT INTO symbols(id, name) VALUES (22, 'Hammer');
-INSERT INTO symbols(id, name) VALUES (23, 'Heart');
-INSERT INTO symbols(id, name) VALUES (24, 'ice cube');
-INSERT INTO symbols(id, name) VALUES (25, 'Igloo');
-INSERT INTO symbols(id, name) VALUES (26, 'Key');
-INSERT INTO symbols(id, name) VALUES (27, 'Ladybug');
-INSERT INTO symbols(id, name) VALUES (28, 'Lightbulb');
-INSERT INTO symbols(id, name) VALUES (29, 'Bolt Lightning');
-INSERT INTO symbols(id, name) VALUES (30, 'lock');
-INSERT INTO symbols(id, name) VALUES (31, 'Arch Blade');
-INSERT INTO symbols(id, name) VALUES (32, 'Moon');
-INSERT INTO symbols(id, name) VALUES (33, 'Entry prohibited sign');
-INSERT INTO symbols(id, name) VALUES (34, 'Orange Scarecrow Man');
-INSERT INTO symbols(id, name) VALUES (35, 'Pencil');
-INSERT INTO symbols(id, name) VALUES (36, 'purple bird');
-INSERT INTO symbols(id, name) VALUES (37, 'cat purple ');
-INSERT INTO symbols(id, name) VALUES (38, 'Purple double hand');
-INSERT INTO symbols(id, name) VALUES (39, 'Red Lips');
-INSERT INTO symbols(id, name) VALUES (40, 'Scissors');
-INSERT INTO symbols(id, name) VALUES (41, 'Skull and Crossbones');
-INSERT INTO symbols(id, name) VALUES (42, 'Snowflake');
-INSERT INTO symbols(id, name) VALUES (43, 'Snowman');
-INSERT INTO symbols(id, name) VALUES (44, 'Spider');
-INSERT INTO symbols(id, name) VALUES (45, 'Cobweb');
-INSERT INTO symbols(id, name) VALUES (46, '​​Sun');
-INSERT INTO symbols(id, name) VALUES (47, 'Sunglasses');
-INSERT INTO symbols(id, name) VALUES (48, 'Target/crosshair');
-INSERT INTO symbols(id, name) VALUES (49, 'Turtle');
-INSERT INTO symbols(id, name) VALUES (50, 'Treble Clef');
-INSERT INTO symbols(id, name) VALUES (51, 'Tree');
-INSERT INTO symbols(id, name) VALUES (52, 'Dripping water');
-INSERT INTO symbols(id, name) VALUES (53, 'Dog');
-INSERT INTO symbols(id, name) VALUES (54, 'Yin and Yang');
-INSERT INTO symbols(id, name) VALUES (55, 'Zebras');
-INSERT INTO symbols(id, name) VALUES (56, 'Question mark');
-INSERT INTO symbols(id, name) VALUES (57, 'cheese');
+-- Gamemodes --
 
 INSERT INTO gamemodes(id, name) VALUES (1, 'The Well');
 INSERT INTO gamemodes(id, name) VALUES (2, 'The Tower');
 INSERT INTO gamemodes(id, name) VALUES (3, 'The Poisoned Gift');
 
-
+-- Initial players, not needed but useful for testing --
 
 INSERT INTO players(id, name) VALUES (1, 'edurobrus');
 INSERT INTO players(id, name) VALUES (2, 'miguel');
@@ -144,6 +97,7 @@ INSERT INTO players(id, name) VALUES (5, 'atci');
 INSERT INTO players(id, name) VALUES (6, 'edus');
 
 
+-- Initial users, not needed but useful for testing --
 
 INSERT INTO USERS(username, password, enabled) VALUES ('User0','p4ssw0rd',TRUE);
 INSERT INTO USERS(username, password, enabled) VALUES ('User1','p4ssw0rd',TRUE);
@@ -164,6 +118,7 @@ INSERT INTO USERS(username, password, enabled) VALUES ('User15','p4ssw0rd',TRUE)
 INSERT INTO USERS(username, password, enabled) VALUES ('User16','p4ssw0rd',TRUE);
 INSERT INTO USERS(username, password, enabled) VALUES ('User17','p4ssw0rd',TRUE);
 
+-- Initial games, not needed but useful for testing --
 
 INSERT INTO games(id, access_code, max_players, state, gamemode_Id, owner_Id, winner_Id) VALUES (1, null, 4, 'LOBBY', 1, 'User17', null);
 INSERT INTO games(id, access_code, max_players, state, gamemode_Id, owner_Id, winner_Id) VALUES (2, null, 6, 'LOBBY', 1, 'User16', null);
@@ -177,7 +132,7 @@ INSERT INTO games(id, access_code, max_players, state, gamemode_Id, owner_Id, wi
 INSERT INTO games(id, access_code, max_players, state, gamemode_Id, owner_Id, winner_Id) VALUES (10, 1234, 2, 'LOBBY', 1, 'User8', null);
 
 
-
+-- Initial users into games --
 INSERT INTO usergames(user_id,game_id) VALUES ('User17',1);
 INSERT INTO usergames(user_id,game_id) VALUES ('User16',2);
 INSERT INTO usergames(user_id,game_id) VALUES ('User15',3);
