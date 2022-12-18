277bdb83b37797c3206db6ad279f5703925ee7d0
marzoiras <marzoiras@alum.us.es>
2022-12-18 08:15:36 +0100

    Registro de usuario

--- src/main/java/org/springframework/samples/dobble/user/UserRepository.java
+++ src/main/java/org/springframework/samples/dobble/user/UserRepository.java
@@ -2,13 +2,26 @@ package org.springframework.samples.dobble.user;
 
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.samples.dobble.model.BaseEntity;
+
 
 
 public interface UserRepository extends  CrudRepository<User, String>{
     
-    @Query("SELECT user FROM User user")
+  
+	/**
+	 * Save an <code>User</code> to the data store, either inserting or updating it.
+	 * @param user the <code>User</code> to save
+	 * @see BaseEntity#isNew
+	 */
+	@SuppressWarnings("unchecked")
+	User save(User user) throws DataAccessException;
+
+	
+	@Query("SELECT user FROM User user")
     List<User> findAll();
 
 
